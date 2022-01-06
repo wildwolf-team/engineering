ef9734b7e6fcba0baa3c649b6ebfc5458f4833c2
Gu-nian <921690547@qq.com>
2022-01-06 10:03:05 +0800

    official demo

---
+++ ncnn_v1.0/build/CMakeFiles/nanodet_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nanodet_demo.dir/main.cpp.o"
+  "CMakeFiles/nanodet_demo.dir/nanodet.cpp.o"
+  "nanodet_demo"
+  "nanodet_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nanodet_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
