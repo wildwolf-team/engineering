4f6af1da79a3694e621dc85458213a30cbb5d29c
Gu-nian <921690547@qq.com>
2022-02-10 20:40:17 +0800

    传统视觉

---
+++ engineer_v2.4/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/oyc/git/helpful/origen_v2.4/function.cpp" "/home/oyc/git/helpful/origen_v2.4/build/CMakeFiles/main.dir/function.cpp.o"
+  "/home/oyc/git/helpful/origen_v2.4/main.cpp" "/home/oyc/git/helpful/origen_v2.4/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/oyc/git/helpful/origen_v2.4/mv_video_capture.cpp" "/home/oyc/git/helpful/origen_v2.4/build/CMakeFiles/main.dir/mv_video_capture.cpp.o"
+  "/home/oyc/git/helpful/origen_v2.4/serialport.cpp" "/home/oyc/git/helpful/origen_v2.4/build/CMakeFiles/main.dir/serialport.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FMT_LOCALE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  "/opt/intel/openvino_2021.4.752/opencv/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ engineer_v2.4/include/configure.h
@@ -0,0 +1,46 @@
+/*
+ * @Author: joyce
+ * @Date: 2021-01-21 14:15:25
+ * @LastEditTime: 2021-01-21 14:15:26
+ * @LastEditors: Please set LastEditors
+ * @Description:: 
+ */
+
+/*-------------------------------------------*/
+
+
+/*---- OpenCV header files ----*/
+#include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/dnn/shape_utils.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+
+/*-----------------------------*/
+
+
+/*---- Others header files ----*/
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+/*------------------------------*/
+
+
+/*---- Serial header files ----*/
+#include <string.h>
+#include <fcntl.h>   //文件控制定义
+#include <termios.h> //POSIX终端控制定义
+#include <unistd.h>  //UNIX标准定义
+#include <errno.h>   //ERROR数字定义
+#include <sys/select.h>
+/*------------------------------*/
+
+using namespace cv;
+using namespace std;
